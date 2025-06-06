8fea6415a0df9a1ca5bf811803b6e9120f9f8b43
Lionjj <danilo.ivone.18@gmail.com>
2025-06-06 17:35:21 +0200

    Fix bug visualizzazione della chat nel caso un recruiter non abbia un azienda e dei lavori

--- resources/views/components/chat-form.blade.php
+++ resources/views/components/chat-form.blade.php
@@ -19,7 +19,7 @@
     </form>
 
         {{-- Form proposta colloquio --}}
-        @if(Auth::user()->hasRole('recruiter'))
+        @if(Auth::user()->hasRole('recruiter') && empty(Auth::user()->company()) && empty(Auth::user()->company()->jobs))
             <hr class="border-gray-200">
             <div x-data="{ open: false }">
                 <button @click="open = !open"
